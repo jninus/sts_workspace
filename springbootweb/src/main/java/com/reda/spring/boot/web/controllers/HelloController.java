67a0a1506365a166c3580fc091ecd8d031cc4a02
jninus <reda.zejli@gmail.com>
2019-11-12 16:05:30 +0100

    first commit

---
+++ springbootweb/src/main/java/com/reda/spring/boot/web/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.reda.spring.boot.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String greet(@RequestParam String username) {
+		return "hello " + username;
+	}
+
+}
